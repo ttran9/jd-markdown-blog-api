c6f73984eed05ec495b0e3818c2da2cbe0028701
ttran9 <toddtran9@gmail.com>
2020-08-18 23:52:07 -0700

    configuring media files for local development.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,8 @@ urlpatterns = [
     path('api/', include('posts.urls', namespace='posts'))
     # path('accounts/', include('allauth.urls')),
 ]
+
+# for local development include media files.
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
