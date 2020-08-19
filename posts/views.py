696325892073eedc39929900183bfc27a603aea5
ttran9 <toddtran9@gmail.com>
2020-08-18 23:10:34 -0700

    posts app.

---
+++ posts/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
+
+
+# Create your views here.
+class PostListView(generics.ListAPIView):
+    permission_classes = [permissions.AllowAny]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
