46c88a560acc3824cd28b52d1a8b23824a1c7d04
ttran9 <toddtran9@gmail.com>
2020-08-21 00:05:22 -0700

    creating a view to handle the post create request.

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostCreateSerializer, PostSerializer
 
 
 # Create your views here.
@@ -15,3 +15,12 @@ class PostDetailView(generics.RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     lookup_field = 'slug'
+
+
+class PostCreateView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = PostCreateSerializer
+
+    def perform_create(self, serializer):
+        # we need to pass the user so we can save with the associated user when we create a post.
+        serializer.save(user=self.request.user)
