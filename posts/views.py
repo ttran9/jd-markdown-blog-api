0b61491467ef343b3f2cd134f6c31d0db73a0010
ttran9 <toddtran9@gmail.com>
2020-08-22 13:18:20 -0700

    creating endpoint for update.

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions
 from .models import Post
-from .serializers import PostCreateSerializer, PostSerializer
+from .serializers import PostCreateSerializer, PostSerializer, PostUpdateSerializer
 
 
 # Create your views here.
@@ -24,3 +24,10 @@ class PostCreateView(generics.CreateAPIView):
     def perform_create(self, serializer):
         # we need to pass the user so we can save with the associated user when we create a post.
         serializer.save(user=self.request.user)
+
+
+class PostUpdateView(generics.UpdateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = PostUpdateSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
