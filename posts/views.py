198b91f773ac0d135d3a21ba391f736eb367f506
ttran9 <toddtran9@gmail.com>
2020-08-24 00:41:22 -0700

    getting context into our view and eventually to the serializer.

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostCreateSerializer, PostSerializer, PostUpdateSerializer
+from .permissions import IsAuthor
 
 
 # Create your views here.
@@ -16,9 +17,16 @@ class PostDetailView(generics.RetrieveAPIView):
     serializer_class = PostSerializer
     lookup_field = 'slug'
 
+    def get_serializer_context(self):
+        context = super(PostDetailView, self).get_serializer_context()
+        context.update({
+            "request": self.request
+        })
+        return context
+
 
 class PostCreateView(generics.CreateAPIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = PostCreateSerializer
 
     def perform_create(self, serializer):
@@ -27,13 +35,13 @@ class PostCreateView(generics.CreateAPIView):
 
 
 class PostUpdateView(generics.UpdateAPIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated, IsAuthor]
     serializer_class = PostUpdateSerializer
     queryset = Post.objects.all()
     lookup_field = 'slug'
 
 
 class PostDeleteView(generics.DestroyAPIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated, IsAuthor]
     queryset = Post.objects.all()
     lookup_field = 'slug'
